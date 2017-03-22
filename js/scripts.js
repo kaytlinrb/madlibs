9196b83ee3e2ad95e89689b9d080d9092e6e906d
Kaytlin Borel <kaytlinrb@gmail.com>
2017-03-21 18:33:35 -0700

    put inputs from form into story

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
 $(function() {
   $("#blanks form").submit(function(event) {
-    $('.person1').append("blah blah");
-    $('.person2').append("blah blah");
-    $('.person1').append("blah blah");
-    $('.animal').append("blah blah");
-    $('.exclamation').append("blah blah");
-    $('.verb').append("blah blah");
-    $('.noun').append("blah blah");
+    var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    var animalInput = $("input#animal").val();
+    var exclamationInput = $("input#exclamation").val();
+    var verbInput = $("input#verb").val();
+    var noun1Input = $("input#noun").val();
+
+    $('.person1').append("person1Input");
+    $('.person2').append("person2Input");
+    $('.animal').append("animalInput");
+    $('.exclamation').append("exclamationInput");
+    $('.verb').append("verbInput");
+    $('.noun').append("nounInput");
 
     $("#story").show();
     event.preventDefault();
